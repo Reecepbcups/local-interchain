a51ba04e698575948cc84d81a963eaa52c4a5c80
Reece Williams <reecepbcups@gmail.com>
2023-06-03 23:27:00 -0500

    Remove encoding stuff

--- src/local-ic/encoding.go
+++ src/local-ic/encoding.go
@@ -6,9 +6,9 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos"
 
 	// Encoding Types
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
-	feesharetypes "github.com/CosmosContracts/juno/v15/x/feeshare/types"
-	tokenfactorytypes "github.com/CosmosContracts/juno/v15/x/tokenfactory/types"
+	// wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	// feesharetypes "github.com/CosmosContracts/juno/v15/x/feeshare/types"
+	// tokenfactorytypes "github.com/CosmosContracts/juno/v15/x/tokenfactory/types"
 
 	// IBC
 	ibccoreclienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
@@ -19,9 +19,9 @@ func LegacyJunoEncoding() *sdktestutil.TestEncodingConfig {
 	cfg := cosmos.DefaultEncoding()
 
 	// register custom types
-	wasmtypes.RegisterInterfaces(cfg.InterfaceRegistry)
-	feesharetypes.RegisterInterfaces(cfg.InterfaceRegistry)
-	tokenfactorytypes.RegisterInterfaces(cfg.InterfaceRegistry)
+	// wasmtypes.RegisterInterfaces(cfg.InterfaceRegistry)
+	// feesharetypes.RegisterInterfaces(cfg.InterfaceRegistry)
+	// tokenfactorytypes.RegisterInterfaces(cfg.InterfaceRegistry)
 
 	return &cfg
 }
@@ -33,15 +33,15 @@ func NewEncoding(options []string) *sdktestutil.TestEncodingConfig {
 	for _, option := range options {
 		if option == "juno" {
 			// Contains all the sub types as well. maybe use a builder for this.
-			feesharetypes.RegisterInterfaces(cfg.InterfaceRegistry)
-			tokenfactorytypes.RegisterInterfaces(cfg.InterfaceRegistry)
-			wasmtypes.RegisterInterfaces(cfg.InterfaceRegistry)
+			// feesharetypes.RegisterInterfaces(cfg.InterfaceRegistry)
+			// tokenfactorytypes.RegisterInterfaces(cfg.InterfaceRegistry)
+			// wasmtypes.RegisterInterfaces(cfg.InterfaceRegistry)
 			ibccoretypes.RegisterInterfaces(cfg.InterfaceRegistry)
 			ibccoreclienttypes.RegisterInterfaces(cfg.InterfaceRegistry)
 		}
 
 		if option == "wasm" {
-			wasmtypes.RegisterInterfaces(cfg.InterfaceRegistry)
+			// wasmtypes.RegisterInterfaces(cfg.InterfaceRegistry)
 		}
 
 		if option == "ibc" {
